708d8714a2017b04a37fef2a9c91b04f68789ea0
Erkan Kantemirov <erkankantemirov@gmail.com>
2023-08-08 22:50:47 +0600

    Initial Commit

---
+++ UberFuber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UberFuber
+//
+//  Created by Erkan Kantemirov on 8/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
